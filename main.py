e6b80af5fcde5955bba9d6b25f9e4d289528c226
Dedet <windyasadewa@gmail.com>
2021-05-27 00:11:17 +0700

    add requirements.txt modif main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import requests
+
 print('Hello Pluto')
+
+try:
+    rl = requests.get('https://google.com')
+    print(rl.status_code)
+    if rl.status_code == 200:
+        print(rl.text)
+except Exception as e:
+    print('ada error', e)
