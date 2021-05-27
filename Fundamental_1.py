71e62f3e61421ae385173810b45ede7d4ec32de3
Dedet <windyasadewa@gmail.com>
2021-05-27 23:57:17 +0700

    add Fundamental_1.py

---
+++ Fundamental_1.py
@@ -0,0 +1,23 @@
+# Sequential = Perintah berurutan
+
+print('Hello Pluto!')
+print('by dedet007')
+RL = '27 Mei 2021'
+print(RL)
+print('-' * len(RL))
+
+# Percabangan = conditional
+
+lisa = True
+
+if lisa:
+    print('bangun siang, di rumah saja')
+else:
+    print('bangun pagi, jalan-jalan bersama pluto')
+
+# Pengulangan = loop
+jml_kucing = 5
+
+for index in range(0,jml_kucing):
+    print(f'Hai anak kucing #{index+1}')
+
